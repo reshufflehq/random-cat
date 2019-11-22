fd5ac55e7c61d6375d47bb1babb42f7e8d7a9541
josephriosio <blankworldwide@gmail.com>
2019-11-22 12:35:50 -0800

    Removed container sinced not used

--- src/components/PreviewFrame.js
+++ src/components/PreviewFrame.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import '@reshuffle/code-transform/macro';
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import InputCopy from './InputCopy';
